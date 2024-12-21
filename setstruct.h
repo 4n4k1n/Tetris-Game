a4b189f6339f34ba4d78b3d0a80bae8f64578562
4n4k1n <anakin@pregitzer.de>
2024-12-21 17:45:44 +0100

    struct to save the settings

---
+++ setstruct.h
@@ -0,0 +1,14 @@
+#ifndef SETSTRUCT_H
+#define SETSTRUCT_H
+
+typedef struct settings
+{
+    int left;
+    int right;
+    int rotate;
+    int soft;
+    int hard;
+    int hold;
+}Settings;
+
+#endif
