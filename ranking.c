6f055321872445aac6f23b7d89668e1015a8bb48
4n4k1n <anakin@pregitzer.de>
2024-12-22 14:26:15 +0100

    stuff for highscore system

---
+++ ranking.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+int check_highscore(char *filename, int score)
+{
+    FILE *fp;
+	int highscore;
+
+	fp = fopen(filename, "r");
+	if (!fp)
+		return (-1);
+	if (fscanf(fp, "%d", &highscore) == 1)
+	{
+		if (score > highscore)
+		{
+			fclose(fp);
+			fp = fopen(filename, "w");
+			fprintf(fp, "%d", score);
+		}
+		fclose(fp);
+		return (highscore);
+	}
+	fclose(fp);
+	return (-1);
+}
---
+++ ranking.h
@@ -0,0 +1,6 @@
+#ifndef RANKING_H
+#define RANKING_H
+
+int check_highscore(char *filename, int score);
+
+#endif
