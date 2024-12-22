0757bbf46eb00d5ca9dc4caa750136ecd7d588f3
4n4k1n <anakin@pregitzer.de>
2024-12-22 19:50:36 +0100

    valid check

--- valid.h
+++ valid.h
@@ -6,5 +6,6 @@
 int piece_is_valid(char **field, ActivePiece piece, char action);
 void check_rows(char **field, int height, int width, int *points);
 void check_game_status(char **field, int *gameover, int width);
+void egg(void);
 
 #endif
