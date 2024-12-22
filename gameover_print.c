95f3d310b44bfcc0666d388e592716f52590d751
4n4k1n <anakin@pregitzer.de>
2024-12-22 16:08:11 +0100

    print the gameover sign

---
+++ gameover_print.c
@@ -0,0 +1,25 @@
+#include <ncurses.h>
+
+void gameover_sign(int points, int highscore)
+{
+    char end;
+
+    clear();
+    printw("  __ _  __ _ _ __ ___   ___  _____   _____ _ __ \n");
+    printw(" / _` |/ _` | '_ ` _ \\ / _ \\/ _ \\ \\ / / _ \\ '__|\n");
+    printw("| (_| | (_| | | | | | |  __/ (_) \\ V /  __/ |  \n");
+    printw(" \\__, |\\__,_|_| |_| |_|\\___|\\___/ \\_/ \\___|_| \n");
+    printw("  __/ |                               @SKYW4LK3R\n");
+    printw(" |___/   \n\n");
+    if (points > highscore)
+        printw("NEW HIGHSCORE >>%d<<\n\n", points);
+    else
+        printw("HIGHSCORE >>%d<<\nSCORE >%d<\n\n", highscore, points);
+    printw("Press 'ESC' to quit.\n");
+    refresh();
+    while (end != 27)
+    {
+        timeout(50000);
+        end = getch();
+    }
+}
---
+++ gameover_print.h
@@ -0,0 +1,6 @@
+#ifndef GAMEOVER_PRINT_H
+#define GAMEOVER_PRINT_H
+
+void gameover_sign(int points, int highscore);
+
+#endif
