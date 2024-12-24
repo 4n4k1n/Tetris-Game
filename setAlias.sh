3204148a96f71406b76158986fc9e7e266dd32cd
4n4k1n <anakin@pregitzer.de>
2024-12-24 15:33:20 +0100

    Set a alias

---
+++ setAlias.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+org_dir=$PWD
+
+cd ~/
+
+chmod 777 .bashrc
+chmod 777 .zshrc
+
+echo "alias tetris='cd "$org_dir" && make clean && make && ./tetris'" >> ~/.bash_aliases
+echo "alias tetris='cd "$org_dir" && make clean && make && ./tetris'" >> ~/.zshrc
