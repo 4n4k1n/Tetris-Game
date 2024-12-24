3c977e3306cb602fde3c1541c0388a6a3683884c
4n4k1n <anakin@pregitzer.de>
2024-12-24 15:46:32 +0100

    script to set a alias

--- setAlias.sh
+++ setAlias.sh
@@ -9,3 +9,5 @@ chmod 777 .zshrc
 
 echo "alias tetris='cd "$org_dir" && make clean && make && ./tetris'" >> ~/.bash_aliases
 echo "alias tetris='cd "$org_dir" && make clean && make && ./tetris'" >> ~/.zshrc
+
+cd $org_dir
