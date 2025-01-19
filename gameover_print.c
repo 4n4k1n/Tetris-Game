aad20404057159215585feaaa4bf61062432b04e
4n4k1n <anakin@pregitzer.de>
2025-01-19 18:29:03 +0100

    added the function for the username input and changed the gameover print function

--- gameover_print.c
+++ gameover_print.c
@@ -1,6 +1,16 @@
 #include <ncurses.h>
+#include <string.h>
+#include "ranking.h"
 
-void gameover_sign(int points, int highscore)
+void get_player_name(Player *current_player)
+{
+    clear();
+    printw("ENTER PLAYER NAME: ");
+    scanf(" %49s", current_player->name);
+    refresh();
+}
+
+void gameover_sign(Player current_player, Player best_player)
 {
     char end;
 
@@ -12,11 +22,28 @@ void gameover_sign(int points, int highscore)
     printw(" \\__, |\\__,_|_| |_| |_|\\___|\\___/ \\_/ \\___|_| \n");
     printw("  __/ |                               @SKYW4LK3R\n");
     printw(" |___/   \n\n");
-    if (points > highscore)
-        printw("NEW HIGHSCORE >>%d<<\n\n", points);
+
+    if (strcmp(best_player.name, current_player.name) == 0)
+    {
+        if (current_player.score > best_player.score)
+            printw("NEW HIGHSCORE >>%d<<\n", current_player.score);
+        else
+            printw(
+                "HIGHSCORE >>%d<<\n"
+                "SCORE >>%d<<\n",
+                best_player.score, current_player.score);
+    }
     else
-        printw("HIGHSCORE >>%d<<\nSCORE >%d<\n\n", highscore, points);
-    printw("Press 'ESC' to quit.\n");
+    {
+        if (current_player.score > best_player.score)
+            printw("NEW HIGHSCORE >>%d<<\n", current_player.score);
+        else
+            printw(
+                "HIGHSCORE >>%d<< FROM '%s'\n"
+                "SCORE >>%d<<\n",
+                best_player.score, best_player.name, current_player.score);
+    }
+    printw("\nPress 'ESC' to quit.\n");
     refresh();
     while (end != 27)
     {
